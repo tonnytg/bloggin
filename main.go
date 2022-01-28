55a1be165b79e846bac8fd5ff280c04fe781ed42
Antonio Thomacelli <tonnytg@gmail.com>
2022-01-28 17:44:27 -0300

    feat: add cmd to edit server mode

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"bloggin/pkg/cmd"
 	"bloggin/pkg/logger"
 	webserver "bloggin/pkg/web"
 )
 
 func main() {
+
+	// Check cmd
+	cmd.Flags()
+
 	logger.Msg("INFO","Starting server...")
+
+	// Start Web server GIN
 	webserver.StartServer()
 }
