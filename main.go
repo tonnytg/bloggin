33f8c87c5773addceaf85437d8ae0f5f966fbabf
Antonio Thomacelli <tonnytg@gmail.com>
2022-01-31 23:57:12 -0300

    feat: add database init

--- main.go
+++ main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"bloggin/pkg/cmd"
+	"bloggin/pkg/database"
 	"bloggin/pkg/logger"
 	webserver "bloggin/pkg/web"
 )
+
+func Init() {
+	// Database init
+	database.InitDatabase()
+}
 
 func main() {
 
 	// Check cmd
 	cmd.Flags()
 
-	logger.Msg("INFO","Starting server...")
+	logger.Msg("INFO", "Starting server...")
 
 	// Start Web server GIN
 	webserver.StartServer()
