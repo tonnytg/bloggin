c1413dd758ea7b8624ccc04accac9b6408a7cc57
Antonio Thomacelli <tonnytg@gmail.com>
2022-02-01 08:23:08 -0300

    feat: add logger and more errors check

--- pkg/database/conf.go
+++ pkg/database/conf.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"bloggin/pkg/logger"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -16,16 +17,21 @@ type DBConfg struct {
 }
 
 // Config is the configuration for the database package
-func Config() *DBConfg{
-	file, _ := os.Open("conf.json")
+func Config() *DBConfg {
+
+	var conf DBConfg
+	file, err := os.Open("./config/database.json")
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer file.Close()
 
 	decoder := json.NewDecoder(file)
-	conf := DBConfg{}
-	err := decoder.Decode(&conf)
+	err = decoder.Decode(&conf)
 	if err != nil {
 		fmt.Println("error db conf:", err)
+		msg := fmt.Sprintf("error db conf: %s", err)
+		logger.Msg("ERROR", msg)
 	}
-
 	return &conf
 }
